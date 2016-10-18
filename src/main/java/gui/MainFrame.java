ef9f2fe2cd2065468bb4fc9f1f5f889bbcfceab9
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-18 19:43:54 +0200

    Finalized Calculator and GUI

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -1,12 +1,35 @@
+/***
+* MainFrame class
+* @author: Carina Ekström
+* @version: 1.0
+**/
+
 package gui;
 
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import otherclasses.Calculator;
 
 public class MainFrame {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JLabel lblResult = new JLabel("");
+	private Calculator calc = new Calculator();
+	private DecimalFormat df = new DecimalFormat("#,###.##", new DecimalFormatSymbols(new Locale("sv", "SE")));
 
 	/**
 	 * Launch the application.
@@ -36,8 +59,95 @@ public class MainFrame {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setBackground(Color.WHITE);
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNumber = new JLabel("Number 1:");
+		lblNumber.setBounds(50, 30, 75, 15);
+		frame.getContentPane().add(lblNumber);
+		
+		JLabel lblNumber_1 = new JLabel("Number 2:");
+		lblNumber_1.setBounds(50, 60, 75, 15);
+		frame.getContentPane().add(lblNumber_1);
+		
+		textField = new JTextField();
+		textField.setBounds(170, 30, 114, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(170, 60, 114, 19);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton button = new JButton("+");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Double result = calc.add(Double.parseDouble(textField.getText()), Double.parseDouble(textField_1.getText()));
+					lblResult.setText(df.format(result).toString());
+				}catch(NumberFormatException e1){
+					lblResult.setText("Use . for decimal numbers");
+				}
+			}
+		});
+		button.setBounds(85, 106, 117, 25);
+		frame.getContentPane().add(button);
+		
+		JButton button_1 = new JButton("-");
+		button_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Double result = calc.subtract(Double.parseDouble(textField.getText()), Double.parseDouble(textField_1.getText()));
+					lblResult.setText(df.format(result).toString());
+				}catch(NumberFormatException e1){
+					lblResult.setText("Use . for decimal numbers");
+				}
+
+			}
+		});
+		button_1.setBounds(248, 106, 117, 25);
+		frame.getContentPane().add(button_1);
+		
+		JButton button_2 = new JButton("*");
+		button_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Double result = calc.multiply(Double.parseDouble(textField.getText()), Double.parseDouble(textField_1.getText()));
+					lblResult.setText(df.format(result).toString());
+				}catch(NumberFormatException e1){
+					lblResult.setText("Use . for decimal numbers");
+				}
+
+			}
+		});
+		button_2.setBounds(85, 156, 117, 25);
+		frame.getContentPane().add(button_2);
+		
+		JButton button_3 = new JButton("/");
+		button_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try{
+					Double result = calc.divide(Double.parseDouble(textField.getText()), Double.parseDouble(textField_1.getText()));
+					lblResult.setText(df.format(result).toString());
+				}catch(NumberFormatException e1){
+					lblResult.setText("Use . for decimal numbers");
+				}
+
+			}
+		});
+		button_3.setBounds(248, 156, 117, 25);
+		frame.getContentPane().add(button_3);
+		
+		JLabel lblResultText = new JLabel("Result:");
+		lblResultText.setBounds(50, 217, 70, 15);
+		frame.getContentPane().add(lblResultText);
+		lblResult.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		lblResult.setBounds(115, 217, 270, 15);
+		frame.getContentPane().add(lblResult);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-
 }
--- src/main/java/otherclasses/Calculator.java
+++ src/main/java/otherclasses/Calculator.java
@@ -1,19 +1,48 @@
+/***
+* Calculator class
+* @author: Carina Ekström
+* @version: 1.0
+**/
 package otherclasses;
 
 public class Calculator {
 
+	/**
+	 * Adds two double values
+	 * @param num1 Value to add
+	 * @param num2 Value to add
+	 * @return Sum of num1 and num2
+	 */
 	public double add(double num1, double num2){
 		return num1 + num2;
 	}
 	
+	/**
+	 * Subtracts two double values
+	 * @param num1 Value to subtract
+	 * @param num2 Value to subtract
+	 * @return Difference between num1 and num2
+	 */
 	public double subtract(double num1, double num2){
 		return num1 - num2;
 	}
 	
+	/**
+	 * Multiplies two double values
+	 * @param num1 Value to multiply
+	 * @param num2 Value to multiply
+	 * @return Product of num1 and num2
+	 */
 	public double multiply(double num1, double num2){
 		return num1 * num2;
 	}
 	
+	/**
+	 * Divides two double values
+	 * @param num1 Value to divide
+	 * @param num2 Value to divide
+	 * @return Quotient between num1 and num2
+	 */
 	public double divide(double num1, double num2){
 		return num1/num2;
 	}
